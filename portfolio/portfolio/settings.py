18526addb425bf083793ba5214df42b655a14853
Denis Kurian Deepu <deniskuriandeepu@gmail.com>
2025-06-15 00:13:29 +0530

    First deployment trial 2 with hostconfiguration

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+h*kwl*0nh9#(ga&5aimgb&^(h5=ziu)l$@_r_lfb)1kpl5_4='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://deniskuriandeepu-portfolio.onrender.com"]
 
 
 # Application definition
